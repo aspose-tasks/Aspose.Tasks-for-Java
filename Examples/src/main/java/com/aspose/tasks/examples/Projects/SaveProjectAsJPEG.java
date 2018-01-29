7d1befdd306701e08322c8e7e5b672c3d850af6a
Kashif Iqbal <kashifiqb>
2018-01-29 19:23:43 +0500

    Updated code samples to Aspose.Tasks for Java 17.11 Release

---
+++ Examples/src/main/java/com/aspose/tasks/examples/Projects/SaveProjectAsJPEG.java
@@ -0,0 +1,28 @@
+package com.aspose.tasks.examples.Projects;
+
+import com.aspose.tasks.ImageSaveOptions;
+import com.aspose.tasks.Project;
+import com.aspose.tasks.SaveFileFormat;
+import com.aspose.tasks.examples.Utils;
+
+public class SaveProjectAsJPEG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 // The path to the documents directory.
+        String dataDir = Utils.getDataDir(SaveAsPdf.class);
+        
+        //ExStart: SaveProjectAsJPEG
+		Project project = new Project(dataDir + "Homemoveplan.mpp");
+
+		// in order to manipulate JPEG quality one can use ImageSaveOptions.JpegQuality property.
+		// The allowed value range is 0..100.
+		ImageSaveOptions options = new ImageSaveOptions(SaveFileFormat.JPEG);
+		options.setJpegQuality(50);
+
+		project.save(dataDir + "image_out.jpeg", options);
+		//ExEnd: SaveProjectAsJPEG
+	}
+
+}
